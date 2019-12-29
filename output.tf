565c8130a735f7b32376546b66c9a8d1f267bceb
omer <omerb@codevalue.net>
2019-12-29 14:37:15 +0200

    added outputs

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "resource_group_name" {
+  value = var.create_resource_group ? azurerm_resource_group.rg[0].name : var.resource_group_name
+}
+
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
+}
+
+output "vnet_name" {
+  value = azurerm_virtual_network.vnet.name
+}
+
+output "vnet_address_space" {
+  value = azurerm_virtual_network.vnet.address_space
+}
